ab7eb994d2dc28db473536705baa1b317815b0ca
이민규 <leeminkyu@imingyuui-MacBookPro.local>
2024-06-09 22:36:52 +0900

    #1 membership management

---
+++ member-spring/src/main/java/member/member_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package member.member_spring.repository;
+
+import member.member_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+
+}
